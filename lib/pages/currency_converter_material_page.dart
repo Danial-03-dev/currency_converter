116ce03982366c00923b12e969339ecffae94e73
Danial-03-dev <danialzafarwork003@gmail.com>
2025-11-20 11:30:33 +0500

    feat: create basic home page widget

---
+++ lib/pages/currency_converter_material_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatelessWidget {
+  const CurrencyConverterMaterialPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: const Text("Hello World 2")));
+  }
+}
