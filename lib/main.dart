7e7d672e90fd34830423b94706d1ef988e54da0a
Danial-03-dev <danialzafarwork003@gmail.com>
2025-11-20 17:03:56 +0500

    fix: homePage name and import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:currency_converter/pages/currency_converter_material_page.dart';
+import 'package:currency_converter/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: CurrencyConverterMaterialPage());
+    return const MaterialApp(home: HomePage());
   }
 }
